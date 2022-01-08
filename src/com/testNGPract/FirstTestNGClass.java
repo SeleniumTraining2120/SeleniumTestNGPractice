073d37a21d2b7626079277dae61cd3ee24f99ec6
seleniumTraining2120 <seleniumtraining2120@gmail.com>
2022-01-08 20:21:47 +0530

    second commit

--- src/com/testNGPract/FirstTestNGClass.java
+++ src/com/testNGPract/FirstTestNGClass.java
@@ -1,9 +1,4 @@
 package com.testNGPract;
-import java.io.FileInputStream;
-import java.util.Properties;
-
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
